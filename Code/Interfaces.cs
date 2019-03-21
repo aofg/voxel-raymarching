e00e99575b6d39ced4a0936e274b600f90acf83a
Aler Denisov <aler.zampillo@gmail.com>
2019-03-22 02:09:46 +0700

    add baker interfaces

---
+++ Code/Interfaces.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace VoxelRaymarching
+{
+    public interface IBakerTaskBuilder
+    {
+        IBakerTaskBuilder AddLayer(ChunkVolumeLayer layer);
+        void Bake();
+    }
+    
+    public interface IVolumeBaker
+    {
+        IBakerTaskBuilder CreateTask(Material targetMaterial);
+    }
+}
