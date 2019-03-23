17336b9260633087f83fdd2d1d5e28f3cebf4f43
Aler Denisov <aler.zampillo@gmail.com>
2019-03-23 22:49:43 +0700

    draft volume baker

--- Code/BufferVolumeBaker.cs
+++ Code/BufferVolumeBaker.cs
@@ -1,61 +1,84 @@
 ﻿using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace VoxelRaymarching
 {
-    public class BufferVolumeTaskBuilder : IBakerTaskBuilder
+    public class BufferVolumeTaskBuilder // : IBakerTaskBuilder
     {
         private const int MAX_LAYERS_COUNT = 64;
         
-        private ChunkVolumeLayer[] _layers;
-        private int _layersCount;
-        private readonly BufferVolumeBaker _baker;
-        private Material _target;
+        internal readonly ChunkVolumeLayer[] layers;
+        internal int layersCount;
+        internal int pointer;
+        internal ComputeBuffer target;
 
-        public BufferVolumeTaskBuilder(BufferVolumeBaker baker)
+
+        public BufferVolumeTaskBuilder()
         {
-            _layers = new ChunkVolumeLayer[MAX_LAYERS_COUNT];
-            _baker = baker;
+            layers = new ChunkVolumeLayer[MAX_LAYERS_COUNT];
         }
 
-        internal void Initialize(Material target)
+        internal void Initialize(ComputeBuffer target, int pointer)
         {
-            _layersCount = 0;
-            _target = target;
+            layersCount = 0;
+            this.target = target;
+            this.pointer = pointer;
         }
         
-        public IBakerTaskBuilder AddLayer(ChunkVolumeLayer layer)
+        public BufferVolumeTaskBuilder AddLayer(ChunkVolumeLayer layer)
         {
-            _layers[_layersCount++] = layer;
-            
+            layers[layersCount++] = layer;
             return this;
         }
 
-        public unsafe void Bake()
-        {
-            _baker.InternalBake(_layers, _layersCount, _target);
-
-            // release to allow GC make dirty work :D
-            _target = null;
-            
-            _baker.ReleaseBuilder(this);
-        }
+//        public void Bake()
+//        {
+//        }
     }
     
-    public class BufferVolumeBaker : IVolumeBaker
+    public class BufferVolumeBaker : MonoBehaviour
     {
+        public const int SIDE = 32;
+        public const int SIDE_2 = 32 * 32;
+        public const int SIDE_3 = 32 * 32 * 32;
+        
+        public ComputeShader ClearShader;
+        public ComputeShader BlendShader;
+        
         private Stack<BufferVolumeTaskBuilder> _pooledBuilders;
+        private Queue<BufferVolumeTaskBuilder> _enqueueTasks;
+        private int itt;
 
         public BufferVolumeBaker()
         {
             _pooledBuilders = new Stack<BufferVolumeTaskBuilder>();
+            _enqueueTasks = new Queue<BufferVolumeTaskBuilder>();
         }
-        
-        public IBakerTaskBuilder CreateTask(Material targetMaterial)
+
+        void Update()
+        {
+            var batch = 8;
+
+            while (_enqueueTasks.Count > 0 && batch > 0)
+            {
+                var task = _enqueueTasks.Dequeue();
+                InternalBake(task.layers, task.layersCount, task.target, task.pointer);
+                ReleaseBuilder(task);
+                batch--;
+            }
+        }
+
+        public BufferVolumeTaskBuilder CreateTask(ComputeBuffer buffer, int pointer)
         {
             var task = GetNextBuilder();
-            task.Initialize(targetMaterial);
+            task.Initialize(buffer, pointer);
             return task;
+        }
+
+        public void DeployTask(BufferVolumeTaskBuilder task)
+        {
+            _enqueueTasks.Enqueue(task);
         }
 
         private BufferVolumeTaskBuilder GetNextBuilder()
@@ -66,16 +89,36 @@ namespace VoxelRaymarching
                 return builder;
             }
             
-            return new BufferVolumeTaskBuilder(this);
+            return new BufferVolumeTaskBuilder();
         }
 
-        internal void InternalBake(ChunkVolumeLayer[] layers, int layersCount, Material target)
+        internal void InternalBake(ChunkVolumeLayer[] layers, int layersCount, ComputeBuffer cb, int pointer)
         {
-            
+            ClearShader.SetBuffer(0, "output", cb);
+            ClearShader.SetInt("length", 32 * 32 * 32);
+            ClearShader.SetInt("ptr", pointer);
+            ClearShader.Dispatch(0, 32, 1, 1); // 1024 cols
+
+            BlendShader.SetBuffer(0, "output", cb);
+            BlendShader.SetInt("ptr", pointer);
+
+
+            for (int index = 0; index < layersCount; index++)
+            {
+                var layer = layers[index];
+                
+                BlendShader.SetInts("inputOffset", layer.Offset.x, layer.Offset.y,  layer.Offset.z);
+                BlendShader.SetInts("inputSize", layer.VolumeSize.x, layer.VolumeSize.y,  layer.VolumeSize.z);
+                BlendShader.SetMatrix("inputTRS", Matrix4x4.Rotate(Quaternion.Euler((float3) layer.Rotate)));
+                BlendShader.SetInts("inputPivot", layer.VolumePivot.x, layer.VolumePivot.y,  layer.VolumePivot.z);
+                BlendShader.SetTexture(0, "input", layer.LayerVolume);
+                BlendShader.Dispatch(0, 1, 1, SIDE);
+            }
         }
 
         internal void ReleaseBuilder(BufferVolumeTaskBuilder bufferVolumeTaskBuilder)
         {
+            bufferVolumeTaskBuilder.target = null;
             _pooledBuilders.Push(bufferVolumeTaskBuilder);
         }
     }
